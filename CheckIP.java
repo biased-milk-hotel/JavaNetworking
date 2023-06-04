0a108239f9ec73a165256b8559b17ea96818eac9
biased-milk-hotel <biased-milk-hotel@users.noreply.github.com>
2023-06-04 18:21:23 -0400

    rm comment

--- CheckIP.java
+++ CheckIP.java
@@ -3,8 +3,7 @@ import java.net.UnknownHostException;
 
 public class CheckIP {
     public static void main(String[] args) throws UnknownHostException {
-        String ipAddress = "192.0.2.1"; // Replace with the IP address you want to check
-
+        String ipAddress = "192.0.2.1";
         InetAddress address = InetAddress.getByName(ipAddress);
         if (address instanceof java.net.Inet4Address) {
             System.out.println(ipAddress + " is an IPv4 address.");
